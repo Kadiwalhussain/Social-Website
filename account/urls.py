1a58859fb89cf9973b959d4ddbd1b3452df205b6
Hussain <hussainkadiwal100@gmail.com>
2025-10-02 18:38:06 +0530

    Completed till Login Page

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 app_name = 'account'
@@ -8,6 +9,17 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(
+             template_name='registration/password_change_form.html',
+             success_url=reverse_lazy('account:password_change_done')
+         ),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='registration/password_change_done.html'
+         ),
+         name='password_change_done'),
 
     path('password-change/',
          auth_views.PasswordChangeView.as_view(),
