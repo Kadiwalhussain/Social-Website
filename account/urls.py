dca1af687e6baca6d02dd7416dd75349fba306c8
Hussain <hussainkadiwal100@gmail.com>
2025-10-02 18:24:27 +0530

    Successfully working till login and logout

--- account/urls.py
+++ account/urls.py
@@ -5,11 +5,10 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
-
-    path ('', views.dashboard, name='dashboard'),
+    # path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
 
 
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
