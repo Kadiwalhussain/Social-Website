8f3a265a986783ac5f71a06e1d1e2176fb1b3c46
Hussain <hussainkadiwal100@gmail.com>
2025-10-03 11:54:58 +0530

    successful working code till user create for all visitor

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from . import views
@@ -6,40 +6,33 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    # Login/Logout
+    # previous login view
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html', next_page='account:dashboard'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    # Password change
-    path('password-change/',
-         auth_views.PasswordChangeView.as_view(
-             template_name='registration/password_change_form.html',
-             success_url=reverse_lazy('account:password_change_done')
-         ),
-         name='password_change'),
-    path('password-change/done/',
-         auth_views.PasswordChangeDoneView.as_view(
-             template_name='registration/password_change_done.html'
-         ),
-         name='password_change_done'),
-
-    # Password reset
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('password-reset/complete/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
-
-    # Dashboard
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    # path('password-change/',
+    # auth_views.PasswordChangeView.as_view(),
+    # name='password_change'),
+    # path('password-change/done/',
+    # auth_views.PasswordChangeDoneView.as_view(),
+    # name='password_change_done'),
+    # reset password urls
+    # path('password-reset/',
+    # auth_views.PasswordResetView.as_view(),
+    # name='password_reset'),
+    # path('password-reset/done/',
+    # auth_views.PasswordResetDoneView.as_view(),
+    # name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/',
+    # auth_views.PasswordResetConfirmView.as_view(),
+    # name='password_reset_confirm'),
+    # path('password-reset/complete/',
+    # auth_views.PasswordResetCompleteView.as_view(),
+    # name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
 
 
--- account/views.py
+++ account/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
 
 
 def user_login(request):
@@ -16,9 +19,11 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -30,3 +35,31 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving yet
+            new_user = user_form.save(commit=False)
+
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+
+            # Save the User object
+            new_user.save()
+
+            return render(
+                request,
+                'account/register_done.html',
+                {'new_user': new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+    )
